618aa3560ef17b1631a87466459fd860b52605fd
Valentin Poupier <poupier_valentin@hotmail.com>
2023-02-20 16:44:05 +0100

    CustomValidator+formFin

--- src/app/modules/demo/formulaires/formulaires.component.ts
+++ src/app/modules/demo/formulaires/formulaires.component.ts
@@ -1,6 +1,6 @@
 import { NotExpr } from '@angular/compiler';
 import { Component } from '@angular/core';
-import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulaires',
@@ -13,7 +13,7 @@ export class FormulairesComponent {
 
   myFormGroup : FormGroup = this._formBuilder.group({
     email : ['',[Validators.email, Validators.required]],
-    username : ['', [Validators.required]],
+    username : ['', [Validators.required, this.moreThanFour()]],
     password : ['', [Validators.required, Validators.minLength(8)]]
   })
 
@@ -23,10 +23,56 @@ export class FormulairesComponent {
 
     console.log(values.username)
 
-    console.log(values['username']);
+    console.log(this.myFormGroup.value['password']);
 
     console.log("j\'ai sususubmit bébé")
+
   }
+
+  // FORM ARRAY
+
+  myFormGroupWithArray : FormGroup = this._formBuilder.group({
+
+    myArray : this._formBuilder.array([
+      new FormControl('', Validators.required)
+    ])
+
+  })
+
+  getArray(){
+    return this.myFormGroupWithArray.get('myArray') as FormArray
+  }
+
+  addControl(){
+    this.getArray().push(new FormControl('',Validators.required))
+  }
+
+  submitArray() {
+    let values = this.myFormGroupWithArray.value;
+
+    console.log(values['myArray']);
+
+    this.getArray().clear()
+
+  }
+
+  removeControl(index : number) {
+    this.getArray().removeAt(index)
+  }
+
+  // CUSTOM Validator
+
+  moreThanFour() : ValidatorFn {
+
+    return (control : AbstractControl) : ValidationErrors | null => {
+
+      let value = control.value;
+      return value.length > 4 ? null : {tabuse : true};
+
+    }
+
+  }
+
 
 
 }
